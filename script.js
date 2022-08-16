e1e1d0334d699a63eb0133dd07fc2e1de9ecc2b6
abdul-rahman-1999 <97880873+abdul-rahman-1999@users.noreply.github.com>
2022-08-16 14:37:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let Myresume = {
+    
+         Name : "Syed Abdul Rahman S",
+         Age : 23,
+         Email : "rahmanabdul10@outlook.com",
+         Mobile : 9600528513,
+         Address : "20C, syed roshan street, panagudi, Tirunelveli District - 627109",
+         Objective : "Looking for a acreer Opportunity to work in a reputated Organization where my skills can be Utilized and enable me to mould my career.",
+         Degree : "BCA (Bachelor of Computer Application) at Muslim Arts College, Thiruvithancode",
+         Course1 : "Graphic and Web design at Image Creative Education, Nagercoil Branch",
+         Course2 : "Full Stack Development at Guvi IIT Madras research park - chennai",    
+        TechnicalSkills : "HTML,css,javascript,React Js,Node Js and Express Js,MongoDB,Mysql,Adobe Photoshop,Adobe Illustrator,aDOBE xd,Figma,UI Design and UX Design",
+        LanguagesKnown : "English,Tamil,Malayalam"
+
+ }
+
+ console.log(JSON.stringify(Myresume));
+
+//  {"Name":"Syed Abdul Rahman S","Age":23,"Email":"rahmanabdul10@outlook.com","Mobile":9600528513,"Address":"20C, syed roshan street, panagudi, Tirunelveli District - 627109","Objective":"Looking for a acreer Opportunity to work in a reputated Organization where my skills can be Utilized and enable me to mould my career.","Education":{"Degree":"BCA (Bachelor of Computer Application) at Muslim Arts College, Thiruvithancode","Course1":"Graphic and Web design at Image Creative Education, Nagercoil Branch","Course2":"Full Stack Development at Guvi IIT Madras research park - chennai"},"TechnicalSkills":"HTML,css,javascript,React Js,Node Js and Express Js,MongoDB,Mysql,Adobe Photoshop,Adobe Illustrator,aDOBE xd,Figma,UI Design and UX Design","LanguagesKnown":"English,Tamil,Malayalam"}
+ 
